61dc43887ff7264882e0f85f4dd215c71bebb7dc
Tony Huang <kuanlin830410@gmail.com>
2025-01-15 23:19:08 +0800

    init commit

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'vue3_final_challenge' // ex: '/the_f2e_2022_week1/'
+    ? '/vue3_final_challenge/' // ex: '/the_f2e_2022_week1/'
     : '/'
 })
