e295f238413363520470e82ded3ff51ac9153964
Tony Huang <kuanlin830410@gmail.com>
2025-01-17 00:02:53 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   // transpileDependencies: true,
-  filenameHashing: false, // 若不希望打包後的檔名出現雜湊值，可加入
+  // filenameHashing: false, // 若不希望打包後的檔名出現雜湊值，可加入
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue3_final_challenge/' // ex: '/the_f2e_2022_week1/'
     : '/'
