85e2bed86dcd9a97a74f0633373c8ef4c663b08f
guanlinhuang <kuanlin830410@gmail.com>
2025-04-23 21:13:22 +0800

    modified files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    'plugin:prettier/recommended' // 這一行加進去
+    "plugin:vue/vue3-essential",
+    "@vue/standard",
+    "eslint:recommended",
+    "plugin:prettier/recommended", // 這一行加進去
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: "@babel/eslint-parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
